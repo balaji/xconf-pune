5a72348d4c5dd1aab5e0901ee887239b64ff4570
Balaji Damodaran <damodaran.balaji@gmail.com>
2010-11-10 23:20:08 +0530

    added a new service project

---
+++ xconf-pune-service/xconf-service/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace XConfPune
+{
+    [ServiceContract]
+    interface IXConfServiceHost
+    {
+        [OperationContract]
+        string dummy();
+    }
+
+    class XConfServiceHost : IXConfServiceHost
+    {
+        public string dummy()
+        {
+            return "Balaji";
+        }
+    }
+
+    class Program
+    {
+        private static ServiceHost HostProxy;
+        static void Main(string[] args)
+        {
+            string address = "http://localhost:8001/xconf";
+            using (HostProxy = new ServiceHost(typeof(XConfServiceHost), new Uri(address)))
+            {
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                smb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
+                HostProxy.Description.Behaviors.Add(smb);
+                HostProxy.Open();
+                Console.WriteLine("The service is ready at " + address);
+                Console.WriteLine("Press <Enter> to stop the service.");
+                Console.ReadLine();
+                HostProxy.Close();           
+            }       
+        }
+    }
+}
