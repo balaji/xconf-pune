20d945596ee09cf065ac908a8bc9a9d5c563d500
Balaji Damodaran <damodaran.balaji@gmail.com>
2010-11-12 10:39:23 +0530

    added missing file

---
+++ xconf-pune-service/xconf-service/XConfSession.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace XConfPune
+{
+    public class XConfSession
+    {
+        public string Topic
+        {
+            get;
+            set;
+        }
+
+        public string Presenters
+        {
+            get;
+            set;
+        }
+
+        public string TimeSlot
+        {
+            get;
+            set;
+        }
+        
+        public int Day
+        {
+            get;
+            set;
+        }
+        
+        public int Track
+        {
+            get;
+            set;
+        }
+    }
+}
