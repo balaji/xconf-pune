e4cedf70cff287d23273f4626418dc381877b7ff
Ramanathan Balakrishnan <sreeramanathan@gmail.com>
2010-11-14 00:35:49 +0530

    service for session details

---
+++ xconf-pune-service/xconf-service/SessionDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XConfPune
+{
+    class SessionDetail
+    {
+        public string Topic
+        {
+            get;
+            set;
+        }
+
+        public string Details
+        {
+            get;
+            set;
+        }
+    }
+}
